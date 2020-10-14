2cb870cb4666823d723ecbb416740eddd37d8f81
hungnv <hungnv@ttc-solutions.c>
2020-10-14 10:34:27 +0700

    use MVVM

--- app/src/main/java/com/example/database/view/MainActivity.kt
+++ app/src/main/java/com/example/database/view/MainActivity.kt
@@ -3,64 +3,70 @@ package com.example.database.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.database.R
 import com.example.database.adapter.StudentAdapter
 import com.example.database.model.Student
 import com.example.database.room.AppDatabase
+import com.example.database.viewmodel.MainActivityViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_student.*
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
-    lateinit var appDatabase: AppDatabase;
     lateinit var studentAdapter: StudentAdapter
-    private var listStudent: ArrayList<Student> =  ArrayList()
+    private var listStudent: ArrayList<Student> = ArrayList()
+
+    private lateinit var viewmodel: MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        appDatabase = AppDatabase.getInstance(this)!!
+        viewmodel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
         initAdapter()
         getDataFromDataBase()
 
         btn_add.setOnClickListener { v ->
-            run {
-                appDatabase.studentDataDao().insertStudent(
-                    student = Student(
-                        edt_id.text.toString(),
-                        edt_name.text.toString(),
-                        edt_sex.text.toString()
-                    )
-                )
-                getDataFromDataBase()
-            }
+            viewmodel.addStudent(
+                edt_id.text.toString(),
+                edt_name.text.toString(),
+                edt_sex.text.toString()
+            )
+            getDataFromDataBase()
+
         }
         btn_delete.setOnClickListener { v ->
-            edt_id.text?.let {
-                appDatabase.studentDataDao().deleteStudent(it.toString())
-            }
 
+            viewmodel.deleteStudent(edt_id.text.toString())
             getDataFromDataBase()
+
+
         }
         btn_edit.setOnClickListener { v ->
-            appDatabase.run {
-                studentDataDao().UpdateStudent(
-                        student = Student(
-                            edt_id.text.toString(),
-                            edt_name.text.toString(),
-                            edt_sex.text.toString()
-                        )
-                    )
-                getDataFromDataBase()
-            }
+
+            viewmodel.ediStudent(
+                edt_id.text.toString(),
+                edt_name.text.toString(),
+                edt_sex.text.toString()
+            )
+            getDataFromDataBase()
+
 
         }
     }
 
+
     fun getDataFromDataBase() {
-        listStudent.clear()
-        Log.e("kevin", "size: "+appDatabase.studentDataDao().getAllStudent()?.size )
-        appDatabase.studentDataDao().getAllStudent()?.let { listStudent.addAll(it) }
-        studentAdapter.notifyDataSetChanged()
+        viewmodel.getAllStudent()
+        viewmodel.requestLiveData.observe(this, Observer { t ->
+            listStudent.clear()
+            listStudent.addAll(t);
+            studentAdapter.notifyDataSetChanged()
+        })
+
     }
 
     fun initAdapter() {
@@ -68,8 +74,7 @@ class MainActivity : AppCompatActivity() {
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         rcv_student.layoutManager = layoutManager
 
-        studentAdapter = StudentAdapter(this, listStudent){
-            student ->
+        studentAdapter = StudentAdapter(this, listStudent) { student ->
             run {
                 edt_id.setText(student.id)
                 edt_name.setText(student.name)
@@ -79,6 +84,6 @@ class MainActivity : AppCompatActivity() {
         }
         rcv_student.adapter = studentAdapter
 
-        }
-
     }
+
+}
--- app/src/main/java/com/example/database/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/database/viewmodel/MainActivityViewModel.kt
@@ -1,9 +1,45 @@
 package com.example.database.viewmodel
 
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.database.model.Student
+import com.example.database.room.AppDatabase
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivityViewModel : ViewModel() {
+class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+    lateinit var appDatabase: AppDatabase;
+
+
+     var requestLiveData: MutableLiveData<List<Student>> = MutableLiveData()
+
+
+    init {
+       appDatabase = AppDatabase.getInstance(application)!!
+    }
+
+    fun addStudent(id: String, name: String, sex: String) {
+        appDatabase.studentDataDao().insertStudent(
+            student = Student(id, name, sex)
+        )
+    }
+
+    fun deleteStudent(id: String) {
+        appDatabase.studentDataDao().deleteStudent(id)
+    }
+
+    fun ediStudent(id: String, name: String, sex: String) {
+        appDatabase.studentDataDao().UpdateStudent(
+            student = Student(id, name, sex)
+        )
+    }
+
+    fun getAllStudent() {
+        requestLiveData.postValue( appDatabase.studentDataDao().getAllStudent()!!)
+    }
 
 
 }
